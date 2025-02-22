fb11278e1034b193363c99493e67070eee998e0c
Ankit <171828455+ankit1350@users.noreply.github.com>
2025-02-22 18:45:09 +0530

    commit

---
+++ codes/love yourself.py
@@ -0,0 +1,19 @@
+print('''all the time that you rain on my parade
+and all the clubs you get in using my name
+you think you broke my heart 
+ohh girl for goodness sake
+you think im crying on my own
+well i ani't
+and i didn't wanna write a song
+cause i didn't want anyone thinking i still care
+Don't but you still hit my phone up
+and baby i'll be moving on
+and i think you should be something
+i dont't wanna hold back
+maybe you should know that
+my mama don't like you and she likes everyone
+and i never like to admit that i was wrong
+and i've been so caught up in my job
+didn't see what's going on
+and now i know 
+i'm bette sleeping on my own''')
