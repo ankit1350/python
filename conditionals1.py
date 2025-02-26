3a92cf0a4070d91674293bb4b8326b51486abe1c
Ankit <171828455+ankit1350@users.noreply.github.com>
2025-02-26 16:44:46 +0530

    Add files via upload

---
+++ conditionals1.py
@@ -0,0 +1,18 @@
+
+age = int(input("Enter your age: "))
+if(age%2==0):
+    print("The entered age is an even number.")
+if (age>=18):
+    print("you are an adult.")
+elif(age<=0):
+    print("the entered age is invalid. Please enter  a valid age number.")
+elif(age==56):
+    print("the age is 56.")
+else:
+    print("you are still a  minor.")
+
+    
+
+
+
+    
