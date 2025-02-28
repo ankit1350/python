c2d603d78c0cbe9078cf7a91728046e6154dc742
ankit1350 <mrankitak135@gmail.com>
2025-02-28 19:55:43 +0530

    initial commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,20 @@
-age=(int(input("enter a valid age number:")))
-
-if (age>=18):
-    print("you are an adult.")
-elif(age<=0):
-    print("the entered age is invalid. Please enter  a valid age number.")
-
-else:
-    print("you are still a  minor.")
-
-    
-
-
-
-    
+print('''summer has already spread in the air
+breezes already blowing,
+the last cold snap is going out
+the days are getting longer and longer
+my days were still going on and on and on,
+i got wet in the sun shower,
+looked up at the night sky,
+it was quite a lonely night ,
+in the blink of on a the dark faded out
+blooming under the sunlight,
+memories with me and you
+all these lights are coloured in by you,
+all these times are precious due to you,
+four seasons i have passed with you,
+four scents were left cause of you,
+uhmmmmm
+on a starry night,
+hope my you sleep tight,
+shining purple light,
+THANKFUL TO BE BY YOUR SIDE NOW.''')
