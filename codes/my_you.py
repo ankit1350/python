e9a79630ee74a3e3d51d766cede773e2368ed093
ankit1350 <171828455+ankit1350@users.noreply.github.com>
2025-03-01 11:44:13 +0530

    Moved my_you.py to codes directory

---
+++ codes/my_you.py
@@ -0,0 +1,20 @@
+print('''summer has already spread in the air
+breezes already blowing,
+the last cold snap is going out
+the days are getting longer and longer
+my days were still going on and on and on,
+i got wet in the sun shower,
+looked up at the night sky,
+it was quite a lonely night ,
+in the blink of on a the dark faded out
+blooming under the sunlight,
+memories with me and you
+all these lights are coloured in by you,
+all these times are precious due to you,
+four seasons i have passed with you,
+four scents were left cause of you,
+uhmmmmm
+on a starry night,
+hope my you sleep tight,
+shining purple light,
+THANKFUL TO BE BY YOUR SIDE NOW.''')
