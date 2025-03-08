ff62c2dca7fc0e02948b6a03f0e638de1c925a70
ankit1350 <171828455+ankit1350@users.noreply.github.com>
2025-03-08 17:55:22 +0530

    new

---
+++ 2002.py
@@ -0,0 +1,15 @@
+print('''I will always remember the day you kissed my lips
+light as a feather ,and it went just like this
+no , it's never been better than the summer of 2002
+we were only eleven, 
+but acting like grownups like we are in the present 
+drinking from plastic cups singing love is forever and ever
+well, I guess that was true
+dancing on the hood in the middle of the woods
+on an old mustang where we sang
+songs with all our childhoos friends
+and it went like this, say
+oops I  got 99 problems singing bye,bye,bye
+hold up, if you wanna go and take a ride with me
+better hit me baby one more time
+''')
