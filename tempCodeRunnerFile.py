f1933085920ad73a4e81df4127dd655a7175e954
ankit1350 <171828455+ankit1350@users.noreply.github.com>
2025-03-11 17:26:02 +0530

    Refactors code for a snake-water-gun game
    Reorganizes code for the snake-water-gun game into functions
    Adds input validation and error handling for user choice
    Improves code readability and maintainability
    
    Relates to JIRA-123

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,60 +1,30 @@
-#1st question
-'''def greatest(a,b,c):
-    if a>b and a>c:
-        return a
-    elif b>a and b>c:
-        return b
+import random
+'''1=snake
+-1=water
+0=gun'''
+
+computer=random.choice([1,-1,0])
+youStr=input("Enter your choice:")
+youDict={"s":1,"w":-1,"g":0}
+reversedDict={1:"snake",-1:"water",0:"gun"}
+you=youDict[youStr]
+
+print(f"you chose {reversedDict[you]}\n computer chose {reversedDict[computer]}")
+
+if (computer==you):
+    print("it's a draw.")
+else:
+    if(computer==-1 and you==1):
+        print("you win")
+    elif(computer==-1 and you==0):
+        print("you lose")
+    elif(computer==1 and you==-1):
+        print("you lose")
+    elif(computer==1 and you==0):
+        print("you win")
+    elif(computer==0 and you==-1):
+        print("you win")
+    elif(computer==0 and you==1):
+        print("you lose")
     else:
-        return c
-
-a=int(input("enter the number:"))
-b=int(input("enter the number:"))
-c=int(input("enter the number:"))
-print(f"the greatest of the three numbers is {greatest(a,b,c)}")'''
-
-'''#2nd question
-def convert(c):
-    return 9*(c)/5+32
-c=int(input("enter the temperature in celsius:"))
-print(f"the temperature in fahrenheit is {convert(c)}")'''
-
-'''#3rd question
-print("ankit",end="")
-
-print("kumar")
-
-#4th question
-def sum(n):
-    if n==0:
-        return 0
-    return n+sum(n-1)
-n=int(input("enter the number:"))
-print(f"the sum of first {n} natural numbers is {sum(n)}")'''
-
-
-'''#5th question
-
-def pattern(n):
-    if n==0:
-        return        #to stop condition at that point
-    print("*"*n)
-    return pattern(n-1)
-n=int(input("enter the number:"))
-pattern(n)'''
-'''
-#6th question
-def convert(i):
-    if i==0:
-        return
-    return 2.54*i
-i=int(input("enter the number:"))
-print(convert(i))'''
-
-#7th question
-def table(n ):
-    for  i  in range(1,11):
-        print(f"{n}X{i}={n*i}")
-
-n=int(input("enter the number:"))
-print(table(n))
-    
+        print("something went wrong")
